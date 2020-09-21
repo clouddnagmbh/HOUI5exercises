2b1c256aced44a99272e606b662fc15bc971ad7f
Maximilian Olzinger <mo@clouddna.at>
2020-09-21 12:49:18 +0000

    Exercise 5

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel",
+	"sap/m/MessageBox",
+	"sap/ui/core/Fragment"
+], function (Controller, JSONModel, MessageBox, Fragment) {
+	"use strict";
+
+	return Controller.extend("at.clouddna.training00.UI5Foundations.controller.App", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"at/clouddna/training00/UI5Foundations/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
